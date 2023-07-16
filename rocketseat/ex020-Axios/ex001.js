f620a4a9f575c3dd0feef169a9ea15ec311a08b0
titansw <jgo179@gmail.com>
2023-07-16 00:45:11 -0300

    .

--- rocketseat/ex020-Axios/ex001.js
+++ rocketseat/ex020-Axios/ex001.js
@@ -1,7 +1,24 @@
 const url = "http://localhost:5500/api"
 
-function getUser(){
+function getUser() {
     axios.get(url)
-    .then()
-    .catch()
+        .then(response => {
+            apiResult.textContent = JSON.stringify(response.data)
+        })
+        .catch(error => console.error(error))
 }
+
+function addNewUser(user) {
+    axios.post(url, user)
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => console.error(error))
+}
+
+const user = {
+    name: "Gracielly",
+    avatar: "https://avatars.githubusercontent.com/u/17316392?v=4", city: "Recife"
+}
+
+getUser()
