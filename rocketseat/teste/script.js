2762927dad2e4f9db7c51d435b7a32a29434a77a
titansw <jgo179@gmail.com>
2023-09-02 00:37:03 -0300

    onlyNumber function

--- rocketseat/teste/script.js
+++ rocketseat/teste/script.js
@@ -1,21 +1,20 @@
-
 function chamado() {
-    const imput = document.getElementById("cep").value
+    const imput = document.getElementById("cep")
     const result = document.createElement("div")
+    const map = `https://www.google.com/maps?q=${imput.value}`
     let linha
-    let map = `https://www.google.com/maps?q=${imput}`
 
     result.classList = "resultado"
 
-    axios.get(`https://viacep.com.br/ws/${imput}/json/`)
+    axios.get(`https://viacep.com.br/ws/${imput.value}/json/`)
         .then(resp => {
             const data = resp.data
 
-            if (Object.keys(data).length < 10) {
-                alert("Digite um CEP válido")
+            if (Object.keys(data) == 'erro') {
+                console.log(Object.keys(data))
+                alert("Cep não encontrado, digite um CEP válido!")
 
             } else {
-
                 document.body.appendChild(result)
 
                 linha = document.createElement("a")
@@ -38,8 +37,14 @@ function chamado() {
             }
         })
         .catch(error => console.error(error))
+    imput.value = ''
 }
 
-
+function onlyNumber(evt, value) {
+    if (value.length === 8) return false
+    return /\d/.test(evt.key)
+}
 
 //  85802000
+//  72231100
+//  01153000
